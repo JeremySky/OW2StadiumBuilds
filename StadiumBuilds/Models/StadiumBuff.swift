25c72d0509874dbd93e55d3345647063118db49f
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2025-11-01 16:11:44 -0700

    CREATE Stadium Buff enum

---
+++ StadiumBuilds/Models/StadiumBuff.swift
@@ -0,0 +1,14 @@
+//
+//  StadiumBuff.swift
+//  StadiumBuilds
+//
+//  Created by Jeremy Manlangit on 11/1/25.
+//
+
+import Foundation
+
+enum StadiumBuff {
+    case weaponPower, weaponLifesteal, attackSpeed, maxAmmo, movementSpeed, increaseDamageToBarriersAndDeployables, criticalDamage
+    case abilityPower, abilityLifesteal, cooldownReduction, startingUltimateCharge
+    case health, armor, shields, negativeEffectDuration, healthArmorShields, knockbackResist, slowResist
+}
